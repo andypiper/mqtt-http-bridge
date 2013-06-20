5566d129f45647bed470bd7c860661e6081f0ce4
Andy Piper <andypiperuk@gmail.com>
2013-06-20 16:47:55 +0100

    Started optimising for mobile

--- mqtt-http-bridge.rb
+++ mqtt-http-bridge.rb
@@ -12,7 +12,8 @@ require 'sinatra'
 class MqttHttpBridge < Sinatra::Base
   MQTT_TIMEOUT = 1.0
   MQTT_OPTS = {
-    :remote_host => 'test.mosquitto.org',
+    :remote_host => 'm2m.eclipse.org',
+    :remote_port => 1883,
     :keep_alive => 2,
     :clean_session => true
   }
